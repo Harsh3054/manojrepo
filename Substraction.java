dcf0b54f2668ad6f676f29bb2ee4cc56b5c43288
Harshal Sandip Patil <harshalspatil21@gmail.com>
2023-04-24 15:56:19 +0530

    Committing subtraction file with changes

--- Substraction.java
+++ Substraction.java
@@ -2,6 +2,6 @@ public class Substraction{
     public static void main(String[] args) {
         int a=895;
         int b=64;
-        System.out.println(a-b);
+        System.out.println("The substraction is"+(a-b));
     }
 }
