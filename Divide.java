65685bd50d9d82a70d35fc3d4ba73cabd134740f
Harshal Sandip Patil <harshalspatil21@gmail.com>
2023-04-24 17:48:32 +0530

    Committing divide file

---
+++ Divide.java
@@ -0,0 +1,8 @@
+public class Divide {
+
+    public static void main(String[] args) {
+        int a=90;
+        int b=10;
+        System.out.println("Multiply branch has divide code"+(a/b));
+    }
+}
